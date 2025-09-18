02300402c50fecf99177c9475326239bcd8d3c35
ErfanSafari0503 <erfyelf@gmail.com>
2025-09-18 05:12:42 +0330

    Implement React Router for application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
-import GlobalStyles from "./styles/GlobalStyles";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+
+const createLazyRoute = (importFn) => {
+  return {
+    lazy: async () => {
+      const { default: Component } = await importFn();
+
+      return {
+        Component: () => <Component />,
+      };
+    },
+  };
+};
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        Component: () => <Navigate replace to="/dashboard" />,
+      },
+      {
+        path: "/dashboard",
+        ...createLazyRoute(() => import("./pages/Dashboard.jsx")),
+      },
+      {
+        path: "/bookings",
+        ...createLazyRoute(() => import("./pages/Bookings.jsx")),
+      },
+      {
+        path: "/cabins",
+        ...createLazyRoute(() => import("./pages/Cabins.jsx")),
+      },
+      {
+        path: "/users",
+        ...createLazyRoute(() => import("./pages/Users.jsx")),
+      },
+      {
+        path: "/settings",
+        ...createLazyRoute(() => import("./pages/Settings.jsx")),
+      },
+      {
+        path: "/account",
+        ...createLazyRoute(() => import("./pages/Account.jsx")),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    ...createLazyRoute(() => import("./pages/Login.jsx")),
+  },
+]);
 
 export default function App() {
-  return (
-    <>
-      <GlobalStyles />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
