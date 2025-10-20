95e0dcb2c02135b585eafcfe210c206522779074
ErfanSafari0503 <erfyelf@gmail.com>
2025-10-20 07:06:45 +0330

    feat: Implement CreateCabinForm component with form validation

---
+++ src/features/cabins/CreateCabinForm.tsx
@@ -0,0 +1,147 @@
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createCabin } from "../../services/apiCabins";
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
+
+interface FormData {
+  name: string;
+  maxCapacity: number;
+  regularPrice: number;
+  discount: number;
+  description: string;
+}
+
+function CreateCabinForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const queryClient = useQueryClient();
+
+  const { isPending: isCreating, mutate } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      toast.success("New cabin successfully created!");
+      reset();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  function onSubmit(data: FormData) {
+    mutate(data);
+  }
+
+  //   function onError(errors) {
+  //     console.log(errors);
+  //   }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Cabin name" id="name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          {...register("name", {
+            required: "Cabin name is required",
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Maximum capacity"
+        id="maxCapacity"
+        error={errors?.maxCapacity?.message}
+      >
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register("maxCapacity", {
+            required: "Maximum capacity is required",
+            min: { value: 1, message: "Capacity must be at least 1" },
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Regular price"
+        id="regularPrice"
+        error={errors?.regularPrice?.message}
+      >
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register("regularPrice", {
+            required: "Regular price is required",
+            min: { value: 50, message: "Price must be at least 50" },
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" id="discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          {...register("discount", {
+            required: "Discount is required",
+            min: { value: 0, message: "Discount cannot be negative" },
+            validate: (value) => {
+              return (
+                value < getValues().regularPrice ||
+                "Discount should be less than the regular price."
+              );
+            },
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Description for website"
+        id="description"
+        error={errors?.description?.message}
+      >
+        <Textarea
+          id="description"
+          defaultValue=""
+          {...register("description", {
+            required: "Description is required",
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin photo" id="image">
+        <FileInput id="image" accept="image/*" disabled={isCreating} />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button disabled={isCreating}>Add Cabin</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -14,6 +14,28 @@ export async function getCabins(): Promise<cabinType[]> {
   return data as cabinType[];
 }
 
+interface newCabinType {
+  name: string;
+  maxCapacity: number;
+  regularPrice: number;
+  discount: number;
+  description: string;
+}
+
+export async function createCabin(newCabin: newCabinType) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created!");
+  }
+
+  return data as cabinType[];
+}
+
 export async function deleteCabin(id: number) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
 
